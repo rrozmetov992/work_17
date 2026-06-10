616ffb742abb82d99ee6a749e38c39639fa5dd0d
rrozmetov992 <rrozmetov992@gmail.com>
2026-06-10 16:46:40 +0300

    Add files via upload

---
+++ work-17.py
@@ -0,0 +1,14 @@
+#17
+# import math
+# x1=int(input("x1="))
+# y1=int(input("y1="))
+# x2=int(input("x2="))
+# y2=int(input("y2="))
+# x3=int(input("x3="))
+# y3=int(input("y3="))
+# print("AB=", math.sqrt((x2-x1)**2+(y2-y1)**2))
+# print("AC=", math.sqrt((x3-x1)**2+(y3-y1)**2))
+# print("BC=", math.sqrt((x3-x2)**2+(y3-y2)**2))
+# print("perimetr P=", math.sqrt((x2-x1)**2+(y2-y1)**2)+
+#       math.sqrt((x3-x1)**2+(y3-y1)**2)+
+#       math.sqrt((x3-x2)**2+(y3-y2)**2))
